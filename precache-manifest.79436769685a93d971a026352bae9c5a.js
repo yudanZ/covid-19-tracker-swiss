fa52189028202a4dcd751a80c4c8e3da92579eaf
yudanZ <zhai.yudan@gmail.com>
2020-10-18 14:49:33 +0200

    Updates

---
+++ precache-manifest.79436769685a93d971a026352bae9c5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc3bfa138236899f03f3598b71402f6",
+    "url": "/covid-19-tracker-swiss/index.html"
+  },
+  {
+    "revision": "ba58493f0d556c9bcb93",
+    "url": "/covid-19-tracker-swiss/static/css/main.144dc6d1.chunk.css"
+  },
+  {
+    "revision": "34e7f24c7c402a1d8948",
+    "url": "/covid-19-tracker-swiss/static/js/2.33b67a10.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/covid-19-tracker-swiss/static/js/2.33b67a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba58493f0d556c9bcb93",
+    "url": "/covid-19-tracker-swiss/static/js/main.a1bb3a8e.chunk.js"
+  },
+  {
+    "revision": "1dd68fb391902a882207",
+    "url": "/covid-19-tracker-swiss/static/js/runtime-main.a0a6bfe7.js"
+  }
+]);
